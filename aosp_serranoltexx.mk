f43f8f92d80ab513782dacff6c1294651bf1db62
harveyspectar <ultimatepravin72@gmail.com>
2017-07-16 11:48:55 +0200

    serranoltexx: Add AOSP Support

--- aosp_serranoltexx.mk
+++ aosp_serranoltexx.mk
@@ -19,10 +19,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from serranoltexx device
 $(call inherit-product, device/samsung/serranoltexx/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common JDC stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_serranoltexx
+PRODUCT_NAME := aosp_serranoltexx
 PRODUCT_DEVICE := serranoltexx
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := Samsung
